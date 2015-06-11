2539ea8842143e7a3baca7011251401244a0051d
KazuSoap <Kazu.Soap@gmail.com>
2015-06-11 19:42:44 +0900

    delete useless settings

--- site-lisp/custom-site-lisp/custom-helm.el
+++ site-lisp/custom-site-lisp/custom-helm.el
@@ -25,20 +25,11 @@
         ;; helm-source-files-in-current-dir
         helm-source-locate))
 
-;; 表示する最大候補数を指定する（デフォルトで 50）
-;; (setq helm-candidate-number-limit 100)
-
-;; 候補表示画面で改行しないようにする
-;; (setq helm-truncate-lines t)
-
 ;; helm-source-buffers-list を詳細に表示しない
 (setq helm-buffer-details-flag nil)
 
 ;; helm-source-buffers-list でバッファ名を表示する幅を調整する
 (setq helm-buffer-max-length 50)
-
-;; helm-swoop で検索結果を cycle しない
-;;(setq helm-swoop-move-to-line-cycle nil)
 
 ;; helm-follow-mode （C-c C-f で ON/OFF）の前回の状態を維持する
 (setq helm-follow-mode-persistent t)
@@ -49,30 +40,6 @@
 ;; http://fukuyama.co/nonexpansion
 ;; 自動補完を無効にする
 (setq helm-ff-auto-update-initial-value nil)
-
-;; C-h でバックスペースと同じように文字を削除できるようにする
-;; (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
-
-;; TAB で補完する
-;;(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
-
-(require 'helm-config)
-;; http://d.hatena.ne.jp/sugyan/20120104/1325604433
-;; プレフィックスキーを C-; に設定する
-(custom-set-variables '(helm-command-prefix-key "C-;"))
-
-;; キーバインドを設定する。コマンド起動後は、以下のキーが利用可能となる
-;;  ・M-n     ：カーソル位置の単語を検索パターンに追加
-;;  ・C-z     ：チラ見
-;;  ・C-c C-f ：helm-follow-mode の ON/OFF
-(global-set-key (kbd "C-x C-b") 'helm-for-files)
-(define-key helm-command-map (kbd "C-;") 'helm-resume)
-(define-key helm-command-map (kbd "y")   'helm-show-kill-ring)
-(define-key helm-command-map (kbd "o")   'helm-occur)
-(define-key helm-command-map (kbd "C-s") 'helm-occur-from-isearch)
-;;(define-key helm-command-map (kbd "s")   'helm-swoop)
-(define-key helm-command-map (kbd "g")   'helm-do-grep) ;;C-u 付で起動すると recursive となる
-(define-key helm-command-map (kbd "t")   'helm-gtags-find-tag)
 
 ;; 先頭に"*helm"が付いたバッファーは表示しない
 (setq helm-boring-buffer-regexp-list '("^*helm"))
