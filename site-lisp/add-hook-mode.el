dcafc0d55e5715aa7bc26d3ad451f88874398ece
KazuSoap <Kazu.Soap@gmail.com>
2015-06-11 17:32:20 +0900

    delete needless hook setting

--- site-lisp/add-hook-mode.el
+++ site-lisp/add-hook-mode.el
@@ -22,47 +22,7 @@
 ;;text-mode
 (add-hook 'text-mode-hook '(lambda ()
 	(font-lock-mode t)
-
-	;; (defface sub-subSect-face '((t (:background "black" :foreground "green"))) nil)
-	;; (defface subSect-face '((t (:background "black" :foreground "#a2d29e"))) nil)
-	;; (defface Sect-face '((t (:background "black" :foreground "gold"))) nil)
-	;; (defface para-face '((t (:foreground "violet"))) nil)
-	;; (defface 0-9-face '((t (:foreground "white"))) nil)
-	;; (defface A-z-face '((t (:background "black" :foreground "#c0d136" ))) nil)
-	;; (defface .-face '((t (:background "#343d55" :foreground "black" ))) nil)
-	;; (defface yaku-face '((t (:foreground "#FF3333"))) nil)
-
-	;; (defvar sub-subSect-face 'sub-subSect-face)
-	;; (defvar subSect-face 'subSect-face)
-	;; (defvar Sect-face 'Sect-face)
-	;; (defvar para-face 'para-face)
-	;; (defvar 0-9-face '0-9-face)
-	;; (defvar A-z-face 'A-z-face)
-	;; (defvar .-face '.-face)
-	;; (defvar yaku-face 'yaku-face)
-
-	;; (defadvice font-lock-mode (before text-font-lock-mode ())
-	;;   (font-lock-add-keywords major-mode '(
-	;; 	( "\\(/\\*sub-.*\\)+\\(\n\\++\\)*" 0 sub-subSect-face append)
-	;; 	( "\\(\\+\\++\\)*" 0 sub-subSect-face append)
-	;; 	( "\\(/\\*s.*\\)+\\(\n~+\\)*" 0 subSect-face append)
-	;; 	( "\\(~~+\\)*" 0 subSect-face append)
-	;; 	( "\\(/\\*.*\\)+\\(\n\\*+\\)*" 0 Sect-face append)
-	;; 	( "\\(\\*\\*+\\)*" 0 Sect-face append)
-	;; 	( "\\(---+\\)*\\(|<.*\\)*" 0 para-face append)
-	;; 	( "[0-9]" 0 0-9-face append)
-	;; 	( "[a-z]*[A-Z]*" 0 A-z-face append)
-	;; 	( "\\." 0 .-face append)
-	;; 	( "||.*" 0 yaku-face append))))
-	;; (ad-enable-advice 'font-lock-mode 'before 'text-font-lock-mode)
-	;; (ad-activate 'font-lock-mode)
 	))
-
-;;emacs-lisp-mode
-;;(add-hook 'emacs-lisp-mode-hook '(lambda () (setq tab-width 4)))
-
-;;sh-mode
-;;(add-hook 'sh-mode-hook '(lambda () (setq tab-width 4)))
 
 ;;c,c++mode
 (autoload 'vs-set-c-style "vs-set-c-style")
