4b7bda28103f8510dd36b941b3d59f0c0b0c4e10
KazuSoap <Kazu.Soap@gmail.com>
2015-07-15 02:03:31 +0900

    add shell-pop settings

--- site-lisp/custom-site-lisp/custom-site-lisp.el
+++ site-lisp/custom-site-lisp/custom-site-lisp.el
@@ -3,9 +3,6 @@
 ;;------------------------------------------------------------------------------
 ;; site-lisp 設定
 ;;------------------------------------------------------------------------------
-;; tabber 設定
-;(load "custom-tabbar")
-
 ;elscreen 設定
 (load "custom-elscreen")
 
@@ -28,9 +25,31 @@
   (setq auto-async-byte-compile-exclude-files-regexp "/elpa/*/*")
   (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
 
+;; sticky shift 設定
 (require 'sticky)
-;(use-sticky-key ?\; sticky-alist:en)    ; for english keyboards
-(use-sticky-key ?\; sticky-alist:ja)    ; for japanese keyboards
+; for japanese keyboards
+(use-sticky-key ?\; sticky-alist:ja)
+
+;; shell-pop 設定
+(global-set-key [f8] 'shell-pop)
+(with-eval-after-load 'shell-pop
+  (defvar shell-pop-internal-mode)
+  (setq shell-pop-internal-mode "shell")
+
+  (defvar shell-pop-internal-mode-buffer)
+  (setq shell-pop-internal-mode-buffer "*shell-pop: shell*")
+
+  (defvar shell-pop-internal-mode-func)
+  (setq shell-pop-internal-mode-func (lambda () (shell)))
+
+  (defvar shell-pop-window-size)
+  (setq shell-pop-window-size 30)
+
+  (defvar shell-pop-full-span)
+  (setq shell-pop-full-span t)
+
+  (defvar shell-pop-window-position)
+  (setq shell-pop-window-position "bottom"))
 
 ;; pop_window 設定
 ;; (load "custom-pop_window")
