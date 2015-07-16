59a7f098904092aa94198681d0fc7e1b10d9f41d
KazuSoap <Kazu.Soap@gmail.com>
2015-07-16 17:43:28 +0900

    add auto-complete-c-headers settings

--- site-lisp/custom-site-lisp/custom-auto-complete.el
+++ site-lisp/custom-site-lisp/custom-auto-complete.el
@@ -3,7 +3,6 @@
 ;;------------------------------------------------------------------------------
 ;; auto-complete
 ;;------------------------------------------------------------------------------
-;;; auto-complete --------------------------------------------------------------
 ;; 補完システム
 ;; from package
 
@@ -11,16 +10,24 @@
 (global-set-key (kbd "M-<RET>") 'auto-complete-mode)
 
 ;; 特定のモードで自動的に有効化
+(defun auto-complete-mode-enable-hooks ()
+  (auto-complete-mode t))
+
 (dolist (hook '(text-mode-hook emacs-lisp-mode-hook
 				sh-mode-hook makefile-mode-hook
 				c-mode-hook c++-mode-hook))
-  (add-hook hook '(lambda () (auto-complete-mode t))))
+  (add-hook hook 'auto-complete-mode-enable-hooks))
 
 ;; 特定のモードで無効化
+(defun auto-complete-mode-disable-hooks ()
+  (auto-complete-mode 0))
+
 (dolist (hook '(lisp-interaction-mode-hook emacs-lisp-byte-code-mode-hook))
-  (add-hook hook '(lambda () (auto-complete-mode 0))))
+  (add-hook hook 'auto-complete-mode-disable-hooks))
 
 (with-eval-after-load 'auto-complete
+  (ac-config-default)
+
   ;; ファイル名の補完をしない
   (defvar ac-sources)
   (defun ac-common-setup ()
@@ -53,3 +60,20 @@
   ;; ポップアップメニューの自動起動
   (defvar ac-auto-show-menu)
   (setq ac-auto-show-menu nil))
+
+;;------------------------------------------------------------------------------
+;; auto-complete-c-headers
+;;------------------------------------------------------------------------------
+;; C/C++ のヘッダの情報源
+;; from package
+
+;; C/C++ モードで自動的に有効化
+(defun auto-complete-c-headers-hooks ()
+  (add-to-list 'ac-sources 'ac-source-c-headers))
+
+(let ((loadfile "auto-complete-c-headers"))
+  (cond ((autoload-if-found 'ac-source-c-headers loadfile t)
+		 (dolist (hook '(c-mode-hook c++-mode-hook))
+		   (add-hook hook 'auto-complete-c-headers-hooks)))
+		(t (display-loading-error-message loadfile))))
+
