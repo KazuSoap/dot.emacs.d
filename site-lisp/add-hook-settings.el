90ddc29781e01ba3b7df8a21d6075fbb02d101d9
KazuSoap <Kazu.Soap@gmail.com>
2015-07-16 17:41:58 +0900

    rm lamda functions

--- site-lisp/add-hook-settings.el
+++ site-lisp/add-hook-settings.el
@@ -16,33 +16,35 @@
 (remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
 
 ;;-- mode設定 --;;
-;;共通設定 (有効化)
+;; 共通設定 (有効化)
+(defun common-mode-enable-hooks ()
+  (setq tab-width 4)
+  (whitespace-mode t)
+  (linum-mode 1)
+  (cua-mode t)
+  (show-paren-mode t))
+
 (dolist (hook '(text-mode-hook emacs-lisp-mode-hook
 				sh-mode-hook makefile-mode-hook
 				c-mode-hook c++-mode-hook))
-  (add-hook hook '(lambda ()
-					(setq tab-width 4)
-					(whitespace-mode t)
-					(linum-mode 1)
-					(cua-mode t)
-					(show-paren-mode t))))
+  (add-hook hook 'common-mode-enable-hooks))
 
-;;共通設定 (無効化)
+;; 共通設定 (無効化)
+(defun common-mode-disable-hooks ()
+  (whitespace-mode 0)
+  (linum-mode 0)
+  (cua-mode 0)
+  (show-paren-mode 0))
+
 (dolist (hook '(esup-mode-hook lisp-interaction-mode-hook
 				emacs-lisp-byte-code-mode-hook))
-  (add-hook hook '(lambda ()
-					(whitespace-mode 0)
-					(linum-mode 0)
-					(cua-mode 0)
-					(show-paren-mode 0))))
+  (add-hook hook 'common-mode-disable-hooks))
 
-;;text-mode
-(add-hook 'text-mode-hook '(lambda ()
-							 (font-lock-mode t)))
+;; c,c++mode
+(defun c_c++-mode-hooks ()
+  (vs-set-c-style))
 
-;;c,c++mode
 (autoload 'vs-set-c-style "vs-set-c-style")
 (dolist (hook '(c-mode-hook c++-mode-hook))
-  (add-hook hook '(lambda ()
-					(vs-set-c-style))))
+  (add-hook hook 'c_c++-mode-hooks))
 
