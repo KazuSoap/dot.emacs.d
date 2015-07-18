f798bf46d4e95c10c3f2bd3c36f06bf6b1f67c98
KazuSoap <Kazu.Soap@gmail.com>
2015-07-18 19:55:39 +0900

    add c++11 settings

--- site-lisp/custom-site-lisp/custom-flycheck.el
+++ site-lisp/custom-site-lisp/custom-flycheck.el
@@ -2,18 +2,28 @@
 
 ;;------------------------------------------------------------------------------
 ;; flycheck 設定
-;; コードチェッカー
+;; エラーチェッカー
 ;; from package
 ;;------------------------------------------------------------------------------
+;; 特定のモードで自動的に有効化
+(defun flycheck-mode-enable-hooks ()
+  (setq left-fringe-width 8) ;; 左フリンジを有効化
+  (flycheck-mode 0)
+  (flycheck-mode t))
 
-;; 特定のモードのみ flycheck-mode と左フリンジを有効
-(dolist (hook '(emacs-lisp-mode-hook c-mode-hook c++-mode-hook))
-  (add-hook hook '(lambda ()
-					(setq left-fringe-width 8) ;; 左フリンジを有効化
-					(flycheck-mode 0)
-					(flycheck-mode t))))
+(dolist (hook '(emacs-lisp-mode-hook sh-mode-hook
+				c-mode-hook c++-mode-hook))
+  (add-hook hook 'flycheck-mode-enable-hooks))
+
+(defun flycheck-c++-mode-hooks ()
+  (defvar flycheck-gcc-language-standard)
+  (setq flycheck-gcc-language-standard "c++11") ;; c++11 でのチェック
+)
+(add-hook 'c++-mode-hook 'flycheck-c++-mode-hooks)
 
 ;; 特定のモードで無効化
+(defun flycheck-mode-disable-hooks ()
+  (flycheck-mode 0))
+
 (dolist (hook '(lisp-interaction-mode-hook emacs-lisp-byte-code-mode-hook))
-  (add-hook hook '(lambda ()
-					(flycheck-mode 0))))
+  (add-hook hook 'flycheck-mode-disable-hooks))
