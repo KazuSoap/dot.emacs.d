faed2a98639c53d5d660d8bc7f30c62f16964529
KazuSoap <Kazu.Soap@gmail.com>
2015-07-23 04:39:15 +0900

    modify indent

--- site-lisp/add-hook-settings.el
+++ site-lisp/add-hook-settings.el
@@ -23,7 +23,6 @@
   (linum-mode 1)
   (cua-mode t)
   (show-paren-mode t))
-
 (dolist (hook '(text-mode-hook emacs-lisp-mode-hook
 				sh-mode-hook makefile-mode-hook
 				c-mode-common-hook))
@@ -35,15 +34,31 @@
   (linum-mode 0)
   (cua-mode 0)
   (show-paren-mode 0))
-
 (dolist (hook '(esup-mode-hook emacs-lisp-byte-code-mode-hook))
   (add-hook hook 'common-mode-disable-hooks))
+
+;; プログラミング言語 共通設定 (有効化)
+(defvar eldoc-idle-delay)
+(defun common-programing-mode-enable-hooks ()
+  (eldoc-mode t)
+  (setq eldoc-idle-delay 0.5))
+(dolist (hook '(emacs-lisp-mode-hook c-mode-common-hook))
+  (add-hook hook 'common-programing-mode-enable-hooks))
+
+;; プログラミング言語 共通設定 (無効化)
+(defun common-programing-mode-disable-hooks ()
+  (eldoc-mode nil)
+  (setq eldoc-idle-delay 0.5))
+(dolist (hook '(esup-mode-hook emacs-lisp-byte-code-mode-hook))
+  (add-hook hook 'common-programing-mode-disable-hooks))
 
 ;; c,c++mode
 (defun c_c++-mode-hooks ()
   (vs-set-c-style))
-
 (autoload 'vs-set-c-style "vs-set-c-style")
 (dolist (hook '(c-mode-hook c++-mode-hook))
   (add-hook hook 'c_c++-mode-hooks))
+
+;; ヘッダファイルを C++ として認識させる
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
