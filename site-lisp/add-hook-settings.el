d00a2674b65389edbbc37236528ffef3db1b41b3
KazuSoap <Kazu.Soap@gmail.com>
2015-07-23 15:10:16 +0900

    disable eldoc-mode in esup-mode

--- site-lisp/add-hook-settings.el
+++ site-lisp/add-hook-settings.el
@@ -7,9 +7,9 @@
 (setq-default major-mode 'text-mode)
 ;
 ;;-- fileの関連付け --;;
-;(setq auto-mode-alist (append (list
-;                               '("\\.txt$" . text-mode)
-;                              auto-mode-alist)))
+(setq auto-mode-alist (append (list
+                              '("\\.h\\'" . c++-mode)
+                             auto-mode-alist)))
 
 ;;-- 不要なhookを外す --;;
 (remove-hook 'find-file-hook 'vc-find-file-hook)
@@ -33,7 +33,8 @@
   (whitespace-mode 0)
   (linum-mode 0)
   (cua-mode 0)
-  (show-paren-mode 0))
+  (show-paren-mode 0)
+  (eldoc-mode 0))
 (dolist (hook '(esup-mode-hook emacs-lisp-byte-code-mode-hook))
   (add-hook hook 'common-mode-disable-hooks))
 
@@ -45,20 +46,9 @@
 (dolist (hook '(emacs-lisp-mode-hook c-mode-common-hook))
   (add-hook hook 'common-programing-mode-enable-hooks))
 
-;; プログラミング言語 共通設定 (無効化)
-(defun common-programing-mode-disable-hooks ()
-  (eldoc-mode nil)
-  (setq eldoc-idle-delay 0.5))
-(dolist (hook '(esup-mode-hook emacs-lisp-byte-code-mode-hook))
-  (add-hook hook 'common-programing-mode-disable-hooks))
-
 ;; c,c++mode
 (defun c_c++-mode-hooks ()
   (vs-set-c-style))
 (autoload 'vs-set-c-style "vs-set-c-style")
 (dolist (hook '(c-mode-hook c++-mode-hook))
   (add-hook hook 'c_c++-mode-hooks))
-
-;; ヘッダファイルを C++ として認識させる
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
