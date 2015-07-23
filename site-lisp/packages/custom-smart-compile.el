89996494bb91f57392f1ea21aef34fc192b74cc9
KazuSoap <Kazu.Soap@gmail.com>
2015-07-23 15:50:15 +0900

    deleted

--- site-lisp/packages/custom-smart-compile.el
+++
@@ -1,9 +0,0 @@
-;;; -*- coding: utf-8; lexical-binding: t -*-
-
-;;------------------------------------------------------------------------------
-;; smart-compile
-;; M-x compile を高機能化
-;; from : package system
-;;------------------------------------------------------------------------------
-;; global-set-key
-(global-set-key (kbd "C-x c") 'smart-compile)
