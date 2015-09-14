5af99626af7ce276b39fbd64c4fd0acb03b3eff5
KazuSoap <Kazu.Soap@gmail.com>
2015-09-15 04:25:14 +0900

    remove useless commands

--- site-lisp/addon/custom-magit.el
+++ site-lisp/addon/custom-magit.el
@@ -8,21 +8,13 @@
 
 (defun ad-magit-read-repository (orig-fun &rest args)
   (let ((ret_val (apply orig-fun args)))
-	(if ret_val (file-truename ret_val))))
+	(if (file-directory-p ret_val) (file-truename ret_val))))
 (advice-add 'magit-read-repository :around 'ad-magit-read-repository)
 
-(autoload 'magit-process-git-arguments "magit-git")
 (defun ad-magit-process-git-arguments (orig-fun &rest args)
-  (mapcar
-   (lambda (x) (replace-regexp-in-string "format=\\(.*\\)" "format='\\1'" x))
+  (mapcar (lambda (x) (replace-regexp-in-string "\\^\\({.*}\\)" "^'\\1'" x))
    (apply orig-fun args)))
 (advice-add 'magit-process-git-arguments :around 'ad-magit-process-git-arguments)
-
-(defvar magit-git-executable)
-(defun ad-magit-git-insert (&rest args)
-  (setq args (magit-process-git-arguments args))
-  (apply #'process-file-shell-command magit-git-executable nil (list t nil) nil args))
-(advice-add 'magit-git-insert :override 'ad-magit-git-insert)
 
 (defun ad-magit-rev-parse|-safe (orig-fun &rest args)
   (let ((ret_val (apply orig-fun args)))
