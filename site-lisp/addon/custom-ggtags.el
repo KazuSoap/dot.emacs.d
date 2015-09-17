f9977b3ef150c4a296f9c2368186ca9a8aec9333
KazuSoap <Kazu.Soap@gmail.com>
2015-09-18 03:06:54 +0900

    renamed function

--- site-lisp/addon/custom-ggtags.el
+++ site-lisp/addon/custom-ggtags.el
@@ -7,18 +7,18 @@
 ;;------------------------------------------------------------------------------
 
 (when (eq system-type 'windows-nt)
-  (defun ggtags-ensure-localname-Win (file)
+  (defun ad-ggtags-ensure-localname (file)
 	"convert Windows path to UNIX path"
 	(cygpath "-u" (and file (or (file-remote-p file 'localname) file))))
-  (advice-add 'ggtags-ensure-localname :override 'ggtags-ensure-localname-Win)
+  (advice-add 'ggtags-ensure-localname :override 'ad-ggtags-ensure-localname)
 
-  (defun ggtags-process-string-Win (orig-func program &rest args)
+  (defun ad-ggtags-process-string (orig-func program &rest args)
 	"if execute global -pr command, convert UNIX path to Windows path"
 	(let((arg (mapconcat 'identity (append (list program) args) nil)))
 	  (cond ((string= "global-pr" arg)
 			 (cygpath "-am" (apply orig-func program args)))
 			(t (apply orig-func program args)))))
-  (advice-add 'ggtags-process-string :around 'ggtags-process-string-Win))
+  (advice-add 'ggtags-process-string :around 'ad-ggtags-process-string))
 
 ;; use helm
 (defvar ggtags-completing-read-function)
