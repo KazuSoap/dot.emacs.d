de44924929266fa8bff3bca5c8e12ada7655feb8
KazuSoap <Kazu.Soap@gmail.com>
2015-09-24 17:09:44 +0900

    add new function my-window-resizer

--- init.el
+++ init.el
@@ -18,9 +18,9 @@
 ;;------------------------------------------------------------------------------
 (dolist (dir '("site-lisp"))
   (let((default-directory (expand-file-name(concat user-emacs-directory dir))))
-	(add-to-list 'load-path default-directory)
-	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-		(normal-top-level-add-subdirs-to-load-path))))
+    (add-to-list 'load-path default-directory)
+    (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+        (normal-top-level-add-subdirs-to-load-path))))
 
 ;;------------------------------------------------------------------------------
 ;; local functions & macro
@@ -28,17 +28,17 @@
 (defun message-startup-time ()
   "echo bootup time in message buffer"
   (message "Emacs loaded in %d ms"
-		   (* 1000 (float-time (time-subtract after-init-time before-init-time)))))
+           (* 1000 (float-time (time-subtract after-init-time before-init-time)))))
 (add-hook 'after-init-hook 'message-startup-time)
 
 (when (eq system-type 'windows-nt)
   (defun cygpath (&optional option path)
-	"cygpath for emacs lisp"
-	(if path (with-temp-buffer
-			   (call-process "d:/msys64/usr/bin/cygpath" nil '(t nil) nil option path)
-			   (unless (bobp)
-				 (goto-char (point-min))
-				 (buffer-substring-no-properties (point) (line-end-position)))))))
+    "cygpath for emacs lisp"
+    (if path (with-temp-buffer
+               (call-process "d:/msys64/usr/bin/cygpath" nil '(t nil) nil option path)
+               (unless (bobp)
+                 (goto-char (point-min))
+                 (buffer-substring-no-properties (point) (line-end-position)))))))
 
 (defun revert-buffer-no-confirm (&optional force-reverting)
   "Interactive call to revert-buffer. Ignoring the auto-save
@@ -46,10 +46,29 @@
  is modified, the command refuses to revert it, unless you specify
  the optional argument: force-reverting to true."
   (interactive "P")
-  ;;(message "force-reverting value is %s" force-reverting)
   (if (or force-reverting (not (buffer-modified-p)))
-	  (revert-buffer :ignore-auto :noconfirm)
-	(error "The buffer has been modified")))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
+
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((dx (if (= (nth 0 (window-edges)) 0) 1 -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1 -1))
+        action c)
+    (message "%s" action)
+    (catch 'end-flag
+      (while t
+        (setq action (read-key-sequence-vector (format "size[%dx%d]" (window-width) (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l) (enlarge-window-horizontally dx))
+              ((= c ?h) (shrink-window-horizontally dx))
+              ((= c ?j) (enlarge-window dy))
+              ((= c ?k) (shrink-window dy))
+              (t (let ((command (key-binding action)))
+                   (when command (call-interactively command)))
+                 (message "Quit")
+                 (throw 'end-flag t)))))))
 
 ;;------------------------------------------------------------------------------
 ;; load files
@@ -65,7 +84,8 @@
 (load "custom-helm")
 (load "custom-irony")
 (load "custom-magit")
-(load "custom-migemo")
+(with-eval-after-load "general-key-bind"
+  (load "custom-migemo"))
 (load "custom-shell-pop")
 (load "custom-twittering-mode")
 
