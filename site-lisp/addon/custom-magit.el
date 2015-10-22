7c7628afede65f2f100ce6dc6c123d827da5bd87
KazuSoap <Kazu.Soap@gmail.com>
2015-10-22 21:36:50 +0900

    remove useless settings

--- site-lisp/addon/custom-magit.el
+++ site-lisp/addon/custom-magit.el
@@ -10,11 +10,4 @@
   (defun ad-magit-read-repository (orig-fun &rest args)
     (let ((ret_val (apply orig-fun args)))
       (if (file-directory-p ret_val) (file-truename ret_val))))
-  (advice-add 'magit-read-repository :around 'ad-magit-read-repository)
-
-  (defun ad-magit-git-str|ing (orig-fun &rest args)
-    (if (and (string= (nth 1 args) "--show-toplevel") (fboundp 'cygpath))
-        (cygpath "-am" (apply orig-fun args))
-      (apply orig-fun args)))
-  (advice-add 'magit-git-str :around 'ad-magit-git-str|ing)
-  (advice-add 'magit-git-string :around 'ad-magit-git-str|ing))
+  (advice-add 'magit-read-repository :around 'ad-magit-read-repository))
