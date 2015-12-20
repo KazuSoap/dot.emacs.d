ad142a0c0e4afe66db7fd5b1e84abe195e60354c
KazuSoap <Kazu.Soap@gmail.com>
2015-12-20 15:50:31 +0900

    byte-compile 時の warning 抑制

--- site-lisp/addon/custom-magit.el
+++ site-lisp/addon/custom-magit.el
@@ -7,7 +7,7 @@
 ;;------------------------------------------------------------------------------
 
 (when (eq system-type 'windows-nt)
-  (defun ad-magit-read-repository (orig-fun &rest args)
+  (defun ad-magit-read-repository (orig-fun &optional args)
     (let ((ret_val (apply orig-fun args)))
       (if (file-directory-p ret_val) (file-truename ret_val))))
   (advice-add 'magit-read-repository :around 'ad-magit-read-repository))
