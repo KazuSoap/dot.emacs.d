6bdf65ca5fc9c1709164b39884a87230bca524da
KazuSoap <Kazu.Soap@gmail.com>
2016-02-16 22:37:32 +0900

    flycheck のエラー表示の遅延を 0.3 に

--- site-lisp/addon/custom-flycheck.el
+++ site-lisp/addon/custom-flycheck.el
@@ -6,6 +6,9 @@
 ;; from package
 ;;------------------------------------------------------------------------------
 
+(defvar flycheck-display-errors-delay)  ;; 遅延
+(setq flycheck-display-errors-delay 0.3)
+
 ;; remove useless warnings
 (with-eval-after-load 'flycheck
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
