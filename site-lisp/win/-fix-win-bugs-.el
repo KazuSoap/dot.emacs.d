c946b27cf86e69a296f9e654df6d0f26d2b4e8db
KazuSoap <Kazu.Soap@gmail.com>
2017-02-22 21:49:44 +0900

    ggtags と irony の nadvice 修正 ggtags: ad-ggtags-ensure-localname の最適化 & ad-ggtags-process-string の修正 irony: ad-ggtags-process-string の最適化

--- site-lisp/win/-fix-win-bugs-.el
+++ site-lisp/win/-fix-win-bugs-.el
@@ -31,17 +31,15 @@
 ;; タグジャンプツール. GNU Global を利用
 ;; from : package system
 ;;------------------------------------------------------------------------------
-(defun ad-ggtags-ensure-localname (orig-func &rest args)
+
+(defun ad-ggtags-ensure-localname (&rest args)
   "convert Windows path to UNIX path"
-  (if (fboundp 'cygpath)
-      (cygpath "-u" (apply orig-func args))
-    (apply orig-func args)))
-(advice-add 'ggtags-ensure-localname :around 'ad-ggtags-ensure-localname)
+  (when (fboundp 'cygpath) (cygpath "-u" (car args))))
+(advice-add 'ggtags-ensure-localname :filter-return 'ad-ggtags-ensure-localname)
 
 (defun ad-ggtags-process-string (orig-func &rest args)
   "if execute global -pr command, convert UNIX path to Windows path"
-  (if (and (string= "global" (car args))
-           (string= "-pr" (nth 1 args))
+  (if (and (string-match "global -p" (format "%s" args))
            (fboundp 'cygpath))
       (cygpath "-am" (apply orig-func args))
     (apply orig-func args)))
@@ -54,8 +52,7 @@
 ;;------------------------------------------------------------------------------
 
 (defun ad-magit-read-repository (args)
-  (when (file-directory-p args)
-    (file-truename args)))
+  (when (file-directory-p args) (file-truename args)))
 (advice-add 'magit-read-repository :filter-return 'ad-magit-read-repository)
 
 ;;------------------------------------------------------------------------------
@@ -68,36 +65,8 @@
 ;; https://github.com/Sarcasm/irony-mode/wiki/Setting-up-irony-mode-on-Windows-using-Msys2-and-Mingw-Packages
 (when (eq system-type 'windows-nt)
   ;; bash on Ubuntu on Windows の irony-server と共存するための設定
-  (defvar irony-server-install-prefix)
-
-  (defun ad-irony--locate-server-executable ()
-    (let* ((irony--server-name "irony-server.exe")
-           (exe (expand-file-name (concat "bin/" irony--server-name) irony-server-install-prefix)))
-      (condition-case err
-          (let ((irony-server-version (car (process-lines exe "--version"))))
-            (if (and (string-match "^irony-server version " irony-server-version)
-                     (version= (irony-version)
-                               (substring irony-server-version
-                                          (length "irony-server version "))))
-                ;; irony-server is working and up-to-date!
-                exe
-              (message "irony-server version mismatch: %s"
-                       (substitute-command-keys
-                        "type `\\[irony-install-server]' to reinstall"))
-              nil))
-        (error
-         (if (file-executable-p exe)
-             ;; failed to execute due to a runtime problem, i.e: libclang.so isn't
-             ;; in the ld paths
-             (message "error: irony-server is broken, good luck buddy! %s"
-                      (error-message-string err))
-           ;; irony-server doesn't exists, first time irony-mode is used? inform
-           ;; the user about how to build the executable
-           (message "%s"
-                    (substitute-command-keys
-                     "Type `\\[irony-install-server]' to install irony-server")))
-         ;; return nil on error
-         nil))))
-  (advice-add 'irony--locate-server-executable :override 'ad-irony--locate-server-executable)
+  (defun ad-irony--locate-server-executable (&rest args)
+    (concat (car args) ".exe"))
+  (advice-add 'irony--locate-server-executable :filter-return 'ad-irony--locate-server-executable)
 
   (setq w32-pipe-read-delay 0))
