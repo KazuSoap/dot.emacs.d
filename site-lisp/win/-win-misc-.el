13fd1c5f5a2dd774673dc9c780e3c5b647f3ee64
KazuSoap <Kazu.Soap@gmail.com>
2017-02-22 22:13:25 +0900

    move process I/O settings from -win-misc- to -fix-win-bugs-

--- site-lisp/win/-win-misc-.el
+++ site-lisp/win/-win-misc-.el
@@ -15,65 +15,6 @@
                (buffer-substring-no-properties (point) (line-end-position))))))
 
 ;;------------------------------------------------------------------------------
-;; process I/O
-;;------------------------------------------------------------------------------
-;; サブプロセスが出力する文字コードを判定して、process-coding-system の設定値を決定
-(setq default-process-coding-system '(undecided-dos . utf-8-unix))
-
-;; サブプロセスに渡すパラメータの文字コードを cp932 にする
-(defmacro set-function-args-encode (fun-name)
-  `(progn
-     (defun ,(intern (format "ad-%s" fun-name)) (args)
-       (mapcar (lambda (arg)
-                 (if (multibyte-string-p arg)
-                     (encode-coding-string arg 'cp932)
-                   arg))
-               args))
-     (advice-add ',fun-name :filter-args ',(intern (format "ad-%s" fun-name)))))
-
-(set-function-args-encode call-process-region)
-(set-function-args-encode call-process)
-(set-function-args-encode start-process)
-
-;; shell バッファがカレントの際、動いている process の coding-system 設定を undecided に
-;; shellバッファで、コマンド実行結果出力前に set-shell-buffer-process-coding-system を実行する。
-;; この設定により、shellバッファで utf-8 の出力をする cygwin コマンドと、cp932 の出力をする
-;; Windowsコマンドの漢字の文字化けが回避される。また、漢字を含むプロンプトが文字化けする場合には、
-;; .bashrc の PS1 の設定の後に「export PS1="$(sleep 0.1)$PS1"」を追加すれば、回避できる模様。
-(defun set-shell-buffer-process-coding-system (&rest args)
-  (let ((process (car args)))
-    (if (and process (string-match "^shell" (process-name process)))
-        (let ((coding-system (process-coding-system process)))
-          (set-process-coding-system process
-                                     (coding-system-change-text-conversion
-                                      (car coding-system) 'undecided)
-                                     (cdr coding-system))))))
-(advice-add 'comint-output-filter :before 'set-shell-buffer-process-coding-system)
-
-;; emacs-24.4 or later において、4096 byte を超えるデータを一度に pipe 経由で
-;; サブプロセスに送り込むと、レスポンスが帰ってこなくなる。
-;; -> 4096 byte ごとにデータを区切って pipe に送るようにする。
-;; 参考：2ch NTEmacsスレッド４ 699 以降
-(defun ad-process-send-string (orig-fun &rest args)
-  (if (not (eq (process-type (car args)) 'real))
-      (apply orig-fun args)
-    (let* ((process (or (car args)
-                        (get-buffer-process (current-buffer))))
-           (send-string (encode-coding-string (nth 1 args)
-                                              (cdr (process-coding-system (get-process process)))))
-           (send-string-length (length send-string)))
-      (let ((w32-pipe-limit 4096)
-            (inhibit-eol-conversion t)
-            (from 0)
-            to)
-        (while (< from send-string-length)
-          (setq to (min (+ from w32-pipe-limit) send-string-length))
-          (setf (nth 1 args) (substring send-string from to))
-          (apply orig-fun args)
-          (setq from to))))))
-(advice-add 'process-send-string :around 'ad-process-send-string)
-
-;;------------------------------------------------------------------------------
 ;; IME
 ;;------------------------------------------------------------------------------
 ;;-- IME customize --;;
