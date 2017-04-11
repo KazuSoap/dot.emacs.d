d94b56ce5388e8fb2ec280710c553469be9ecfbf
KazuSoap <Kazu.Soap@gmail.com>
2017-04-11 23:23:37 +0900

    auto-insert の遅延ロード

--- site-lisp/common/-aute-insert-.el
+++ site-lisp/common/-aute-insert-.el
@@ -6,31 +6,33 @@
 ;; emacs default
 ;;------------------------------------------------------------------------------
 
-(require 'autoinsert)
-
-;; テンプレートのディレクトリ
-(setq auto-insert-directory "~/.emacs.d/auto-insert/")
-
-;; 各ファイルによってテンプレートを切り替える
-(setq auto-insert-alist
-      (nconc '(("\\.cpp$"   . ["template.cpp" my-template])
-               ("\\.h$"     . ["template.h" my-template])
-               ("Makefile$" . ["template.make" my-template]))
-             auto-insert-alist))
-
-;; テンプレート中で展開してほしいテンプレート変数を定義
-(defvar template-replacements-alists)
-(setq template-replacements-alists
-      `(("%file%"             . ,(lambda () (file-name-nondirectory (buffer-file-name))))
-        ("%file-without-ext%" . ,(lambda () (file-name-sans-extension (file-name-nondirectory (buffer-file-name)))))
-        ("%include-guard%"    . ,(lambda () (format "INCLUDE_%s_H" (upcase (file-name-sans-extension (file-name-nondirectory (buffer-file-name)))))))))
-
-(defun my-template ()
-  (time-stamp)
-  (dolist (c template-replacements-alists)
-    (goto-char (point-min))
-    (while (search-forward (car c) nil t)
-      (replace-match (funcall (cdr c)))))
-  (goto-char (point-max))
-  (message "done."))
 (add-hook 'find-file-not-found-hooks 'auto-insert)
+
+(with-eval-after-load 'autoinsert
+  ;; テンプレートのディレクトリ
+  (defvar auto-insert-directory)
+  (setq auto-insert-directory "~/.emacs.d/auto-insert/")
+
+  ;; テンプレート中で展開してほしいテンプレート変数を定義
+  (defvar template-replacements-alists)
+  (setq template-replacements-alists
+        `(("%file%"             . ,(lambda () (file-name-nondirectory (buffer-file-name))))
+          ("%file-without-ext%" . ,(lambda () (file-name-sans-extension (file-name-nondirectory (buffer-file-name)))))
+          ("%include-guard%"    . ,(lambda () (format "INCLUDE_%s_H" (upcase (file-name-sans-extension (file-name-nondirectory (buffer-file-name)))))))))
+
+  (defun my-template ()
+    (time-stamp)
+    (dolist (c template-replacements-alists)
+      (goto-char (point-min))
+      (while (search-forward (car c) nil t)
+        (replace-match (funcall (cdr c)))))
+    (goto-char (point-max))
+    (message "done."))
+
+  ;; 各ファイルによってテンプレートを切り替える
+  (defvar auto-insert-alist)
+  (setq auto-insert-alist
+        (append '(("\\.cpp$"   . ["template.cpp" my-template])
+                  ("\\.h$"     . ["template.h" my-template])
+                  ("Makefile$" . ["template.make" my-template]))
+                auto-insert-alist)))
