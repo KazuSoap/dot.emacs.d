ac4f705615dd5f684a226a80245f866d1a440b31
KazuSoap <Kazu.Soap@gmail.com>
2019-02-05 07:49:53 +0900

    remove useless settings & modify mozc settings

--- site-lisp/misc/-gnu-linux-misc-.el
+++ site-lisp/misc/-gnu-linux-misc-.el
@@ -1,51 +1,18 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 
 ;;------------------------------------------------------------------------------
-;; linux-misc
-;;------------------------------------------------------------------------------
-;;------------------------------------------------------------------------------
 ;; IME
 ;;------------------------------------------------------------------------------
-
 ;; emacs-mozc
-(require 'mozc)
 (setq default-input-method "japanese-mozc")
 
-(require 'mozc-popup)
-(setq mozc-candidate-style 'popup)
+(with-eval-after-load 'mozc
+  (require 'mozc-popup)
+  (setq mozc-candidate-style 'popup))
 
 (global-set-key (kbd "<zenkaku-hankaku>") 'toggle-input-method)
-
-;;------------------------------------------------------------------------------
-;; set-faces
-;;------------------------------------------------------------------------------
-(when (not window-system)
-  ;;-- mode line in active --;;
-  (set-face-attribute 'mode-line nil :foreground "white" :background "DarkRed")
-
-  ;;-- linum-mode --;;
-  (with-eval-after-load 'linum
-    (set-face-attribute 'linum nil :foreground "gray" :background "black" :underline nil))
-
-  ;;-- whitespace --;;
-  (with-eval-after-load 'whitespace
-    ;; space
-    (set-face-attribute 'whitespace-space nil :foreground "GreenYellow" :background "black")
-    ;; tab
-    (set-face-attribute 'whitespace-tab nil :foreground "LightSkyBlue" :background "black" :underline t)
-    ;; newline
-    (set-face-attribute 'whitespace-newline nil :foreground "DeepSkyBlue")
-    ;; trailing
-    (set-face-attribute 'whitespace-trailing nil :background "DeepPink"))
-
-  ;;-- elscreen --;;
-  (with-eval-after-load 'elscreen
-    ;; tab-background (header-line)
-    (set-face-attribute 'elscreen-tab-background-face nil :background "black" :underline nil)
-    ;; tab-current-screen (current-screen)
-    (set-face-attribute 'elscreen-tab-current-screen-face nil :foreground "yellow" :background "black" :underline nil)
-    ;; tab-other-screen (other-screen)
-    (set-face-attribute 'elscreen-tab-other-screen-face nil :foreground "Gray72" :background "black" :underline nil)))
+(global-set-key (kbd "<henkan>") (lambda () (interactive) (activate-input-method default-input-method)))
+(global-set-key (kbd "<muhenkan>") (lambda () (interactive) (deactivate-input-method)))
 
 ;;------------------------------------------------------------------------------
 ;; misc
