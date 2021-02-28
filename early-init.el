6d7b6feec22413515acfb53475343a6374668fdd
KazuSoap <Kazu.Soap@gmail.com>
2021-02-28 17:53:12 +0900

    remove useless settings

--- early-init.el
+++ early-init.el
@@ -22,7 +22,6 @@
          (setenv "MSYSTEM" "MINGW64")
          (or (getenv "SHLVL") (setenv "SHLVL" "0"))
          (setq-default shell-file-name ,(getenv "SHELL"))
-         (setq-default w32-pipe-read-delay 0) ;; Windows performance tweaks
 
          ;; coding-system
          ;; デフォルトの文字コードを設定
